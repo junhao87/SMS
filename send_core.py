c2fa204a960873d00c5781cc64e690ee3e38aa98
junhao87 <138763029+junhao87@users.noreply.github.com>
2026-02-23 15:57:36 +0800

    Update send_core.py

--- send_core.py
+++ send_core.py
@@ -13,6 +13,7 @@ from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 
 from twilio.rest import Client
 
